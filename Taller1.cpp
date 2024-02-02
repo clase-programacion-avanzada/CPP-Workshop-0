8781b513d95e8c0b3f4c56546f101298dd6fd83c
Diego Fernando Mora Velasco <difemove@gmail.com>
2024-02-02 16:45:41 -0500

    add boilerplate

--- Taller1.cpp
+++ Taller1.cpp
@@ -49,10 +49,11 @@ int main() {
 }
 
 void printMenu() {
-    cout << "1. Generar números aleatorios" << endl;
-    cout << "2. Mostrar números sin repetir" << endl;
-    cout << "3. Mostrar números de aparición" << endl;
-    cout << "4. Generar matriz con números aleatorios" << endl;
-    cout << "5. Mostrar valor mínimo por columna" << endl;
+    cout << "1. Crear e imprimir una lista de números enteros de 20 elementos." << endl;
+    cout << "2. Crear e imprimir una lista de números sin repetir." << endl;
+    cout << "3. Crear e imprimir una lista de números con el número de veces que aparece el número en la lista del punto 1." << endl;
+    cout << "4. Generar una matriz de 5x5 con números aleatorios" << endl;
+    cout << "5. Generar una función que imprima los valores mímimos de cada columna, indicando el número de la columna y el valor mínimo." << endl;
     cout << "0. Salir" << endl;
+    cout << "Ingrese una opción: ";
 }
