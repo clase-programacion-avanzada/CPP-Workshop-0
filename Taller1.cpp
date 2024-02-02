0e681d98fcfbd30ce9a87f28660233aca8e846f6
Diego Fernando Mora Velasco <difemove@gmail.com>
2024-02-02 16:32:19 -0500

    add boilerplate

--- Taller1.cpp
+++ Taller1.cpp
@@ -4,55 +4,55 @@
 
 using namespace std;
 
+void printMenu();
+
 int main() {
-    /*En este taller es obligatorio el uso de estructuras y funciones. 
-    Debe usar headers para evitar que el código se vea muy extenso, y no puede usar cin o cout en ninguna parte del código que no sea el archivo main.cpp.
     
-    Se recomienda que use la estructura NumberList para el desarrollo de este taller.
+    printMenu();
+
+    int option = 0;
+    NumberList list;
+    NumberMatrix matrix;
+
+    do {
+        cout << "Ingrese una opción: ";
+        cin >> option;
+
+       switch (option)
+        {
+            case 1:
+                cout << "Opción 1" << endl;
+                break;
+            case 2:
+                cout << "Opción 2" << endl;
+                break;
+            case 3:
+                cout << "Opción 3" << endl;
+                break;
+            case 4:
+                cout << "Opción 4" << endl;
+                break;
+            case 5:
+                cout << "Opción 5" << endl;
+                break;
+            case 0:
+                cout << "Saliendo..." << endl;
+                break;
+            default:
+                cout << "Opción inválida" << endl;
+                break;
+        } 
+
+    } while (option != 0);
     
-    */
-
-
-
-    /*Desarrolle un programa que cuente con un menú que permita las siguientes operaciones:*/
-
-    /*
-    1. Crear e imprimir una lista de números enteros de 20 elementos.
-        Los números deben ser generados de forma aleatoria entre 1 y 10.
-        i.e: 
-            [4,7,2,1,5,3,2,1,9,10,2,3,4,5,6,7,8,9,10,1]
-
-    2. Crear e imprimir una lista de números donde se guardarán los números del 
-        punto anterior una sola vez, es decir, sin repetir ningún número y en el 
-        órden de aparición. 
-        i.e: 
-            [4,7,2,1,5,3,9,10,6,8]
-    3. Crear e imprimir una lista de números donde se guarda en cada posición el 
-        número de veces que aparece el número en la lista del punto 1. 
-        i.e: 
-            [2,2,3,3,2,2,2,2,1,1]
-
-    4. Haciendo uso de la estructura NumberMatrix, generar números aleatorios del 1 al 10, 
-        guardarlos en una matriz de 5x5 e imprimirla. 
-        i.e: 
-            [4,7,2,1,5
-             3,2,1,9,10
-             2,3,4,5,6
-             7,8,9,10,1
-             1,2,3,4,5] 
-    5. Generar una función que imprima los valores mímimos de cada columna, indicando 
-        el número de la columna y el valor mínimo.
-        i.e: 
-            fila columna valor
-            0    3       1
-            1    2       1
-
-
-    */
-    
-     
-    
-
-
     return 0;
+}
+
+void printMenu() {
+    cout << "1. Generar números aleatorios" << endl;
+    cout << "2. Mostrar números sin repetir" << endl;
+    cout << "3. Mostrar números de aparición" << endl;
+    cout << "4. Generar matriz con números aleatorios" << endl;
+    cout << "5. Mostrar valor mínimo por columna" << endl;
+    cout << "0. Salir" << endl;
 }
---
+++ libs/NumberList.h
@@ -0,0 +1,102 @@
+#ifndef NUMBERLIST_H
+#define NUMBERLIST_H
+
+#include <string>
+
+using namespace std;
+
+struct NumberList {
+    int size;
+    int numbers[100];
+
+    NumberList() {
+        size = 0;
+    }
+
+    void add(int number) {
+        numbers[size] = number;
+        size++;
+    }
+
+    void remove(int index) {
+        for (int i = index; i < size - 1; i++) {
+            numbers[i] = numbers[i + 1];
+        }
+        size--;
+    }
+
+    int get(int index) {
+        return numbers[index];
+    }
+
+    bool set(int index, int number) {
+        if (index <= 0 || index > size) {
+            return false;
+        }
+
+        numbers[index] = number;
+        return true;
+        
+    }
+
+    bool contains(int number) {
+        for (int i = 0; i < size; i++) {
+            if (numbers[i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void clear() {
+        size = 0;
+    }
+
+    NumberList copy() {
+        NumberList other;
+        for (int i = 0; i < other.size; i++) {
+            numbers[i] = other.numbers[i];
+        }
+        size = other.size;
+        return other;
+    }
+
+    string toString() {
+        string result = "[";
+        for (int i = 0; i < size; i++) {
+            result += to_string(numbers[i]);
+            if (i < size - 1) {
+                result += ", ";
+            }
+        }
+        result += "]";
+        return result;
+    }
+};
+
+NumberList generateNumberListWithRandomNumbers(int size, int min, int max) {
+    NumberList result;
+    
+    //Acá se debe completar el código
+
+    return result;
+}
+
+NumberList getNumberListWithoutRepetitions(NumberList list) {
+    NumberList result;
+    
+    //Acá se debe completar el código
+
+    return result;
+}
+
+NumberList getNumberListWithNumberOfRepetitions(NumberList list, NumberList listWithoutRepetitions) {
+    NumberList result;
+
+    //Acá se debe completar el código
+
+    return result;
+    
+}
+
+#endif /* NUMBERLIST_H */
